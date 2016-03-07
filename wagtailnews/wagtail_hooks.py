95fc93cb506473536c81e2918b526be06aa7141d
Tim Heap <tim@timheap.me>
2016-03-08 10:55:17 +1100

    Add newsitem permissions to group permission screen
    TODO: Use the ModelPermissionPolicy coming in 1.4 to actually implement
    permissions correctly, instead of just showing/hiding the menu.
    Totes insecure atm.

--- wagtailnews/wagtail_hooks.py
+++ wagtailnews/wagtail_hooks.py
@@ -1,13 +1,15 @@
-from __future__ import unicode_literals, absolute_import
+from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import include, url
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from django.core import urlresolvers
 from django.utils.translation import ugettext_lazy as _
-
-from wagtail.wagtailcore import hooks
 from wagtail.wagtailadmin.menu import MenuItem
+from wagtail.wagtailcore import hooks
 
 from . import urls
+from .models import NEWSINDEX_MODEL_CLASSES
 from .permissions import user_can_edit_news
 
 
@@ -25,3 +27,11 @@ def construct_main_menu(request, menu_items):
             MenuItem(_('News'), urlresolvers.reverse('wagtailnews_choose'),
                      classnames='icon icon-grip', order=250)
         )
+
+
+@hooks.register('register_permissions')
+def newsitem_permissions():
+    newsitem_models = [model.get_newsitem_model()
+                       for model in NEWSINDEX_MODEL_CLASSES]
+    newsitem_cts = ContentType.objects.get_for_models(*newsitem_models).values()
+    return Permission.objects.filter(content_type__in=newsitem_cts)
