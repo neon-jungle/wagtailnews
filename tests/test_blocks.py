e73d4719737825a198cbf05e1f4acb3cf9bce19b
Tim Heap <tim@timheap.me>
2016-10-06 14:52:42 +1100

    Fix NewsChooserBlock.deconstruct and target_model argument
    It was being serialized in the migration as the reference to the model
    itself (app.models.Foo), not the label string ('app.Foo').
    
    Also allow specifying the model as an 'app.Foo' label, as well as the
    literal model reference.

---
+++ tests/test_blocks.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from tests.app.models import NewsItem
+from wagtailnews.blocks import NewsChooserBlock
+
+
+class TestNewsChooserBlock(TestCase):
+    def test_target_model_string(self):
+        block = NewsChooserBlock(target_model='app.NewsItem')
+        self.assertIs(block.target_model, NewsItem)
+
+    def test_target_model_literal(self):
+        block = NewsChooserBlock(target_model=NewsItem)
+        self.assertIs(block.target_model, NewsItem)
+
+    def test_deconstruct(self):
+        block = NewsChooserBlock(target_model=NewsItem)
+        path, args, kwargs = block.deconstruct()
+        self.assertEqual(path, 'wagtailnews.blocks.NewsChooserBlock')
+        self.assertEqual(args, ())
+        self.assertEqual(kwargs, {'target_model': 'app.NewsItem'})
--- wagtailnews/blocks.py
+++ wagtailnews/blocks.py
@@ -2,17 +2,27 @@ from __future__ import absolute_import, unicode_literals
 
 from django.utils.functional import cached_property
 from wagtail.wagtailcore.blocks import ChooserBlock
+from wagtail.wagtailcore.utils import resolve_model_string
 
 
 class NewsChooserBlock(ChooserBlock):
     def __init__(self, target_model, **kwargs):
         super(NewsChooserBlock, self).__init__(**kwargs)
-        self.target_model = target_model
+        self._target_model = target_model
+
+    @cached_property
+    def target_model(self):
+        return resolve_model_string(self._target_model)
 
     @cached_property
     def widget(self):
         from wagtailnews.widgets import AdminNewsChooser
         return AdminNewsChooser(self.target_model)
+
+    def deconstruct(self):
+        path, args, kwargs = super(NewsChooserBlock, self).deconstruct()
+        kwargs['target_model'] = self.target_model._meta.label
+        return path, args, kwargs
 
     class Meta:
         icon = "grip"
