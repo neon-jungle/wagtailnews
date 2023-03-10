ad67c53f98f89d77e1a469a4afa29e12c33e3cad
Juho Rutila <juho.rutila@iki.fi>
2023-03-10 18:12:15 +0200

    chore: remove deprecated providing_args from signals

--- wagtailnews/signals.py
+++ wagtailnews/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import Signal
 
-newsitem_published = Signal(providing_args=['instance', 'created'])
-newsitem_unpublished = Signal(providing_args=['instance'])
-newsitem_draft_saved = Signal(providing_args=['instance', 'created'])
-newsitem_deleted = Signal(providing_args=['instance'])
+newsitem_published = Signal() # instance, created
+newsitem_unpublished = Signal() # instance
+newsitem_draft_saved = Signal() # instance, created
+newsitem_deleted = Signal() # instance
